7504c8476e633167b713ef2e707c9a1d04298185
Parth Agrawal <63773427+mega123-art@users.noreply.github.com>
2025-09-15 05:16:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:3000","http://localhost:5502","https://redorblack.onrender.com"],
+    origin: ["http://localhost:5173", "http://localhost:3000","http://localhost:5502","https://redorblack.onrender.com","https://www.redorblack.fun"],
     methods: ["GET", "POST"],
   },
 });
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000","http://localhost:5502","https://redorblack.onrender.com"],
+    origin: ["http://localhost:5173", "http://localhost:3000","http://localhost:5502","https://redorblack.onrender.com"."https://www.redorblack.fun"],
     credentials: true,
   })
 );
